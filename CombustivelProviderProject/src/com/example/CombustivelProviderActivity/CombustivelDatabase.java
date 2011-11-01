e7fda5c169c91d02d445665f020849758d6b2d9a
edipoterra <edipoterra@gmail.com>
2011-11-01 20:17:36 -0200

    Definido banco de dados

--- CombustivelProviderProject/src/com/example/CombustivelProviderActivity/CombustivelDatabase.java
+++ CombustivelProviderProject/src/com/example/CombustivelProviderActivity/CombustivelDatabase.java
@@ -26,10 +26,47 @@ public class CombustivelDatabase extends SQLiteOpenHelper {
     db.execSQL("CREATE TABLE " + combustivelDB + " ("
         colID + "INTEGER PRIMARY KEY , " + 
         colData + " DATE, " + 
-        colOdometro + "  "
-        )
+        colOdometro + "FLOAT, " + 
+        colLitros + " FLOAT, " + 
+        colCombustivel + " INTEGER)");
   }
 
+  public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    db.execSQL("DROP TABLE IF EXISTS " + combustivelTable);
+    onCreate(db);
+  }
+
+  void adicionaCombustivel(){
+    SQLiteDatabase db = this.getWritableDatabase();
+    ContentValues cv = new ContentValues();
+    cv.put();
+    cv.put();
+    cv.put();
+    db.insert(combustivelTable, colNome, cv);
+    db.close();
+  }
+
+  int getPessoasCount(){
+   SQLiteDatabase db=this.getWritableDatabase();
+   Cursorursor cur= db.rawQuery("Select * from "+agendaTable, null);
+   int x= cur.getCount();
+   cur.close();
+   return x;
+  }
+
+  Cursor getAllPessoas(){
+    SQLiteDatabase db=this.getWritableDatabaseetWritableDatabase();
+    Cursor cur= db.rawQuery("SELECT * FROM "+agendaTable,null);
+    return cur;
+  }
+
+  public Cursor getPessoaPorNome(String nome)
+  {
+    SQLiteDatabase db=this.getReadableDatabase();
+    String [)}) colunas=new String[]{colID,colNome,colAniversario,colFone};
+    Cursor c=db.query(agendaTable, colAniversariounas, colNome+"=?", new String[){nome}, null, null, null);
+    return c; 
+  }
 
 }
 
