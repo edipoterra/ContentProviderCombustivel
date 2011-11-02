45738b4617799bc2db3633843db65f543625ecf1
edipoterra <edipoterra@gmail.com>
2011-11-02 11:15:16 -0200

    Inicio do processo de desenvolvimento no feriado! =P

--- CombustivelProviderProject/src/com/example/CombustivelProviderActivity/CombustivelDatabase.java
+++ CombustivelProviderProject/src/com/example/CombustivelProviderActivity/CombustivelDatabase.java
@@ -8,65 +8,67 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class CombustivelDatabase extends SQLiteOpenHelper {
   
-  static final String dbName = "combustivelDB";
-  static final String combustivelTable = "combustivel";
-  static final String colID = "_id";
-  static final String colData = "data";
-  static final String colOdometro = "odometro";
-  static final String colLitros = "litros";
-  static final String colCombustivel = "combustivel";
-  static final int DATABASE_VERSION = 1;
+  	static final String dbName = "combustivelDB";
+  	static final String combustivelTable = "combustivel";
+  	static final String colID = "_id";
+  	static final String colData = "data";
+  	static final String colOdometro = "odometro";
+  	static final String colLitros = "litros";
+  	static final String colCombustivel = "combustivel";
+  	static final int DATABASE_VERSION = 1;
   
-  public CombustivelDatabase (Context context){
-    super(context, dbName, null, DATABASE_VERSION);
-  }
+  	public CombustivelDatabase (Context context){
+    	super(context, dbName, null, DATABASE_VERSION);
+  	}
 
-  @Override
-  public void onCreate(SQLiteDatabase db) {
-    db.execSQL("CREATE TABLE " + combustivelDB + " ("
-        colID + "INTEGER PRIMARY KEY , " + 
-        colData + " DATE, " + 
-        colOdometro + "FLOAT, " + 
-        colLitros + " FLOAT, " + 
-        colCombustivel + " INTEGER)");
-  }
+  	@Override
+  	public void onCreate(SQLiteDatabase db) {
+    	db.execSQL("CREATE TABLE " + combustivelTable + " ("
+        	colID + "INTEGER PRIMARY KEY , " + 
+        	colData + " DATE, " + 
+        	colOdometro + "FLOAT, " + 
+        	colLitros + " FLOAT, " + 
+        	colCombustivel + " INTEGER)");
+  	}
 
-  public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-    db.execSQL("DROP TABLE IF EXISTS " + combustivelTable);
-    onCreate(db);
-  }
+  	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    	db.execSQL("DROP TABLE IF EXISTS " + combustivelTable);
+    	onCreate(db);
+  	}
 
-  void adicionaCombustivel(){
-    SQLiteDatabase db = this.getWritableDatabase();
-    ContentValues cv = new ContentValues();
-    cv.put();
-    cv.put();
-    cv.put();
-    db.insert(combustivelTable, colNome, cv);
-    db.close();
-  }
+  	void adicionaCombustivel(CombustivelProviderActivity combustivel){
+	   	SQLiteDatabase db = this.getWritableDatabase();
+    	ContentValues cv = new ContentValues();
+    	cv.put(colData, combustivel.getData());
+    	cv.put(colOdometro, combustivel.getOdometro());
+    	cv.put(colLitros, combustivel.getLitros());
+		cv.put(colCombustivel, combustivel.getCombustivel());
+    	db.insert(combustivelTable, colData, cv);
+    	db.close();
+  	}
 
-  int getPessoasCount(){
-   SQLiteDatabase db=this.getWritableDatabase();
-   Cursorursor cur= db.rawQuery("Select * from "+agendaTable, null);
-   int x= cur.getCount();
-   cur.close();
-   return x;
-  }
+  	Cursor getAllCombustivel(){
+    	SQLiteDatabase db=this.getWritableDatabaseetWritableDatabase();
+    	Cursor cur= db.rawQuery("SELECT * FROM "+ combustivelTable,null);
+	    return cur;
+  	}
 
-  Cursor getAllPessoas(){
-    SQLiteDatabase db=this.getWritableDatabaseetWritableDatabase();
-    Cursor cur= db.rawQuery("SELECT * FROM "+agendaTable,null);
-    return cur;
-  }
+	public int atualizaCombustivel(CombustivelProviderActivity combustivel){
+		SQLiteDatabase db=this.getWritableDatabase();
+		ContentValues cv = new ContentValues();
+    	cv.put(colData, combustivel.getData());
+    	cv.put(colOdometro, combustivel.getOdometro());
+    	cv.put(colLitros, combustivel.getLitros());
+		cv.put(colCombustivel, combustivel.getCombustivel());		
+		return db.update(combustivelTable, cv, colID+"=?", new String []{String.valueOf(combustivel.get_id())}); 
+	}
+	 
+	public void removeCombustivel(CombustivelProviderActivity combustivel){
+		SQLiteDatabase db=this.getWritableDatabase();
+		db.delete(combustivelTable,colID+"=?", new String [] {String.valueOf(combustivel.get_id())});
+		db.close();
+	}
 
-  public Cursor getPessoaPorNome(String nome)
-  {
-    SQLiteDatabase db=this.getReadableDatabase();
-    String [)}) colunas=new String[]{colID,colNome,colAniversario,colFone};
-    Cursor c=db.query(agendaTable, colAniversariounas, colNome+"=?", new String[){nome}, null, null, null);
-    return c; 
-  }
 
 }
 
--- CombustivelProviderProject/src/com/example/CombustivelProviderActivity/CombustivelProviderActivity.java
+++ CombustivelProviderProject/src/com/example/CombustivelProviderActivity/CombustivelProviderActivity.java
@@ -1,15 +1,27 @@
 package com.example.CombustivelProviderActivity;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class CombustivelProviderActivity extends Activity
-{
+
+public class CombustivelProviderActivity extends Activity {
+	protected EditText txtData;
+	protected EditText txtOdometro;
+	protected EditText txtLitros;
+	//protected Spinner  comboCombustiveis;
+	protected CombustivelDatabase dbCombustivel;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+		
     }
 }
